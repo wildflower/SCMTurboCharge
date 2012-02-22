326b6158177a763f4c0d1336d52c656d2fb6381b
Haydn <wildflower1975@gmail.com>
2012-02-23 10:31:33 +1300

    adding files

--- no_RDs.sql
+++ no_RDs.sql
@@ -7,19 +7,10 @@ AND reportmonth > "201101"
 GROUP BY icp; 
 
 SELECT 
-    champion_registry.icp
+    champion_registry.icp, champion_registry.metercontact, retailer
 FROM
     champion_registry
         LEFT JOIN
     RD_status ON champion_registry.icp = RD_status.icp
 WHERE
     RD_status.icp IS NULL and champion_registry.icpstatus = 2;
-
-select 
-    champion_registry.icp
-from
-    RD_status
-        right join
-    _registry USING (icp)
-where
-    icpstatus = 2 and RD_status.icp is NULL;
