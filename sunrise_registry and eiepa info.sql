e234a52582eed23bc845c8e60df2063de4dd0ee8
Joy <joy@scmgroup.co.nz>
2012-06-13 15:50:29 +1200

    left join testing

--- sunrise_registry and eiepa info.sql
+++ sunrise_registry and eiepa info.sql
@@ -1,5 +1,5 @@
 SELECT 
-    sunrise_registry.icp, distpricecat,  sunrise_eiep1.reportmonth,    sunrise_eiep1.unittype,    sunrise_eiep1.units,sunrise_eiep1.status, sunrise_eiep1.fixedvariable,
+    sunrise_registry.icp, distpricecat, sunrise_eiep1.startdate,sunrise_eiep1.enddate, sunrise_eiep1.reportmonth,    sunrise_eiep1.unittype,    sunrise_eiep1.units,sunrise_eiep1.status, sunrise_eiep1.fixedvariable,
     sunrise_eiep1.chargeabledays,    sunrise_eiep1.pricecode,    sunrise_eiep1.pricerate,    sunrise_eiep1.charge,
      sunrise_eiep1.retailer,sunrise_registry.retailer as 'Current Retailer',metercontact,chargeablecapacity,     addressnumber,  
      addressstreet,    addresstown, reference ,addressuserref,   icpstatus,    category,    metertypehhr,    ami,
@@ -8,7 +8,8 @@ FROM
     scm.sunrise_registry
         inner join
     sunrise_eiep1 ON sunrise_registry.icp = sunrise_eiep1.icp
-    where  sunrise_eiep1.icp ='1000023619BPDE8'
+    where  sunrise_eiep1.icp ='1000006434BPB0C' 
+
     order by sunrise_eiep1.reportmonth desc
 
     
