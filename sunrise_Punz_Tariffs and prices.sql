8e4337016cbbd13dfff825b7a967d9477d674d1c
Joy <joy@scmgroup.co.nz>
2012-06-18 11:06:25 +1200

    new sql

--- sunrise_Punz_Tariffs and prices.sql
+++ sunrise_Punz_Tariffs and prices.sql
@@ -1,5 +1,5 @@
 select
-icp,retailer, reportmonth, tariffs.Price as 'Tariff',
+sunrise_registry.icp,distpricecat, sunrise_eiep1.retailer,  reportmonth, fixedvariable,tariffs.Price as 'Ret Tariff',
 sunrise_eiep1.pricecode, pricerate
 from sunrise_eiep1 inner join tariffs on tariffs.code = sunrise_eiep1.pricecode 
-and (sunrise_eiep1.startdate > tariffs.StartDate and sunrise_eiep1.startdate < date_add(tariffs.StartDate,interval 1 year)) where retailer = 'PUNZ'
+and (sunrise_eiep1.startdate > tariffs.StartDate and sunrise_eiep1.startdate < date_add(tariffs.StartDate,interval 1 year)) inner join sunrise_registry on sunrise_registry.icp = sunrise_eiep1.icp where sunrise_eiep1.retailer = 'PUNZ'
