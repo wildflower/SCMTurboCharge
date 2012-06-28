a677bc19265ba9c18069087e681d47c4f38ef8ed
Joy <joy@scmgroup.co.nz>
2012-06-28 15:45:45 +1200

    updated sqls

--- champion_registry and eiep1 info_2.sql
+++ champion_registry and eiep1 info_2.sql
@@ -1,5 +1,5 @@
 SELECT 
-    champion_registry.icp,    champion_eiep1.reportmonth,   champion_eiep1.units,    champion_eiep1.status,    champion_eiep1.retailer,
+    champion_registry.icp,champion_eiep1.startdate,champion_eiep1.enddate,    champion_eiep1.reportmonth,   champion_eiep1.units,    champion_eiep1.status,    champion_eiep1.retailer,
     metercontact,    champion_eiep1.pricecode,    champion_eiep1.fixedvariable,    champion_eiep1.pricerate,    champion_eiep1.charge,champion_eiep1.chargeabledays,
     champion_registry.retailer as 'current retailer' , 
     addressuserref,    addressnumber,    addressstreet,    addresstown,    icpstatus,    metertypenhh,
@@ -8,8 +8,8 @@ FROM
     scm.champion_registry         inner join
     champion_eiep1 ON champion_registry.icp = champion_eiep1.icp
 where
-     champion_eiep1.icp = '0000142280WA97E'
-     and champion_eiep1.fixedvariable = 'V'
+      
+    champion_eiep1.icp = '0000225224WAAE7' 
      order by champion_eiep1.reportmonth desc
 
      
